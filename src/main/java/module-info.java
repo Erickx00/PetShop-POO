ba4a1df0bb23ea0fa1e3d524e583473bbdff97a1
Erickx00 <erickdeveloperx10@gmail.com>
2025-12-29 16:15:07 -0300

    Adicionamento do databind para lidar como LocalDataTime e agora tem colocar os caminhos no modulo para evitar erro

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,17 @@ module org.example.petshoppoo {
     requires com.fasterxml.jackson.databind;
     requires java.desktop;
     requires javafx.graphics;
+    requires com.fasterxml.jackson.datatype.jsr310;
 
+    opens org.example.petshoppoo to javafx.fxml;
     opens org.example.petshoppoo.controllers to javafx.fxml;
     opens org.example.petshoppoo.model.Login to com.fasterxml.jackson.databind;
-    opens org.example.petshoppoo to javafx.fxml;
+    opens org.example.petshoppoo.model.Dono to com.fasterxml.jackson.databind;
+    opens org.example.petshoppoo.model.Pet to com.fasterxml.jackson.databind;
+    opens org.example.petshoppoo.model.Usuario to com.fasterxml.jackson.databind;
+
+
+
 
     exports org.example.petshoppoo;
 }
