e652ad7b5695d277919c2ef457b04764c5c194fd
Erickx00 <erickdeveloperx10@gmail.com>
2026-01-22 17:12:29 -0300

    pequena alteracao

--- src/main/java/org/example/petshoppoo/utils/FilePaths.java
+++ src/main/java/org/example/petshoppoo/utils/FilePaths.java
@@ -1,7 +1,7 @@
 package org.example.petshoppoo.utils;
 
 public class FilePaths {
-    public static final String DATA_DIR = "src/main/resources/data/";
+    public static final String DATA_DIR = "data/";
     public static final String USUARIOS_JSON = DATA_DIR + "usuarios.json";
     public static final String PETS_JSON = DATA_DIR + "pets.json";
     public static final String SERVICOS_JSON = DATA_DIR + "servicos.json";
